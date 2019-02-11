e74cfe7fff2d1936e11f126c7be4e1d92774758a
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-02-11 16:12:32 +0000

    first draft

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
+let score = document.querySelector(".scorecounter");
 let holes = document.querySelectorAll(".hole");
-let score = document.querySelectorAll(".score");
 let moles = document.querySelectorAll(".mole");
 
-let totalscore = 0;
+let currentHole;
+let gameHasFinished = false;
 
-//
-
-let randomTime = (min, max) => {
-  return Math.round(Math.Random() * (min - max) + min);
+let startGame = () => {
+  score.textContent = 0;
+  gameHasFinished = false;
+  moleComeUp();
 };
 
-let randomHole = holes => {};
+let randomTime = (max, min) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+let randomHole = holes => {
+  let i = Math.floor(Math.random() * holes.length);
+  let hole = holes[i];
+  if (hole === currentHole) {
+    randomHole();
+  }
+  currentHole = hole;
+  return hole;
+};
+
+let moleComeUp = () => {
+  let time = randomTime(200, 1000);
+  let hole = randomHole(holes);
+  hole.classList.add("move");
+  setTimeout(() => {
+    hole.classList.remove("move");
+    if (!gameHasFinished) moleComeUp();
+  }, time);
+};
