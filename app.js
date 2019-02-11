40209d38a160fbf5d42bd0185279df4df7e30491
Alexander Ormandy <alexanderormandy@Alexanders-MBP.lan>
2019-02-11 13:55:03 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let holes = document.querySelectorAll(".hole");
+let score = document.querySelectorAll(".score");
+let moles = document.querySelectorAll(".mole");
+
+let totalscore = 0;
+
+//
+
+let randomTime = (min, max) => {
+  return Math.round(Math.Random() * (min - max) + min);
+};
+
+let randomHole = holes => {};
